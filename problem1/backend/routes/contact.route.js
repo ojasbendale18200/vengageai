5b5e5e5fa477451403609776a46633b2d3088330
Ojas <ojasbendale18200@gmail.com>
2023-12-01 13:15:42 +0530

    completed all routes

--- problem1/backend/routes/contact.route.js
+++ problem1/backend/routes/contact.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { ContactModel } = require("../model/contact.model");
 const ContactRouter = express.Router();
 
+// Post Contact
 ContactRouter.post("/addcontact", async (req, res) => {
   try {
     const contact = new ContactModel(req.body);
@@ -12,5 +13,70 @@ ContactRouter.post("/addcontact", async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
+
+// Get all contact
+ContactRouter.get("/contact", async (req, res) => {
+    try {
+   
+        const { firstname } = req.query;
+        let query = {};
+    
+        if (firstname) {
+          query = { firstname: new RegExp(firstname, 'i') };
+        }
+    
+        const contacts = await ContactModel.find(query);
+    
+        res.status(200).json(contacts);
+      } catch (error) {
+        console.error("Error getting contacts:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+});
+
+ContactRouter.get("/contact/:id", async (req, res) => {
+    try {
+      const contact = await ContactModel.findById(req.params.id);
+      if (!contact) {
+        return res.status(404).json({ message: "Contact not found" });
+      }
+      res.status(200).json(contact);
+    } catch (error) {
+      console.error("Error getting contact:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  });
+
+  ContactRouter.patch("/contact/:id", async (req, res) => {
+    try {
+      const contact = await ContactModel.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+      if (!contact) {
+        return res.status(404).json({ message: "Contact not found" });
+      }
+      res.status(200).json({ message: "Contact updated successfully", contact });
+    } catch (error) {
+      console.error("Error updating contact:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  });
+  
+  ContactRouter.delete("/contact/:id", async (req, res) => {
+    try {
+      const contact = await ContactModel.findByIdAndDelete(req.params.id);
+      if (!contact) {
+        return res.status(404).json({ message: "Contact not found" });
+      }
+      res.status(200).json({ message: "Contact deleted successfully", contact });
+    } catch (error) {
+      console.error("Error deleting contact:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  });
+  
+  
 
 module.exports = { ContactRouter };
